99cc22b677da20d0205cc44e5528d3eb0be7859c
Souslapluie <77190496+Souslapluie@users.noreply.github.com>
2021-03-27 23:02:25 +0100

    Add files via upload

---
+++ Rayman.py
@@ -0,0 +1,30 @@
+import pygame 
+
+pygame.init()
+
+# Générer fenêtre de jeu
+
+pygame.display.set_caption("Rayman Rev1val")  # display = affichage, set_caption = titre(s)
+screen = pygame.display.set_mode((1080, 720)) # set_mode = affichage de la fenêtre (taille)
+
+# Importer images : 
+
+background = pygame.image.load('Raymages/Rayman1.5.webp') # Attribution d'une image à la variable 'background'
+
+running = True # running = Nom de la boucle while ? While ne marche pas toute seule, booléen strict minimum
+
+while running == True :
+
+	# Application image arrière-plan
+	screen.blit(background, (0, 0))
+
+	#MAJ ecran
+	pygame.display.flip()
+
+	for event in pygame.event.get() :
+		if event.type == pygame.QUIT :
+			running == False
+			pygame.quit()   # Ferme la fenêtre lorsqu'on clique sur la croix (croix marche pas sans cette ligne)
+
+  
+   
